ba4e271ac8410d6b1e726616be06d1299811a57a
Vajkyy <vajki2014@gmail.com>
2025-03-12 12:25:26 +0100

    javitott

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { Kepek } from "./Kepek.js";
 import { KEPEKLISTA } from "./adat.js";
 
-const GALERIAELEM = document.querySelectorAll(".galeria")
+const GALERIAELEM = document.querySelectorAll(".galeria")[0]
 const GALERIAKEP = new Kepek(KEPEKLISTA, GALERIAELEM)
